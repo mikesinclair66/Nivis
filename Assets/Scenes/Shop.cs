b9638666fb81e091b5f9903f13a1bf959ee1ba3e
Raziq Khan <35462830+RaziqK@users.noreply.github.com>
2021-10-11 19:17:43 -0700

    Currency Created - Node.cs still needs editing

---
+++ Assets/Scenes/Shop.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public TurretBlueprint standardTurret;
+    public TurretBlueprint missileLauncher;
+
+    BuildManager buildManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void SelectStandardTurret()
+    {
+        Debug.Log("Standard Turret Selected");
+        buildManager.SelectTurretToBuild(standardTurret);
+    }
+
+    public void SelectMissileLauncher()
+    {
+        Debug.Log("Missile Launcher Selected");
+        buildManager.SelectTurretToBuild(missileLauncher);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
