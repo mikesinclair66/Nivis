9334064c1d92a25b3665212dafb36fbd5bb43665
JackyLau01 <jlau154@my.bcit.ca>
2021-10-13 21:36:48 -0700

    fixed conflicts

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -9,30 +9,43 @@ public class Node : MonoBehaviour
     // TODO: set hover color
     public Color hoverColor;
     public Vector3 positionOffset = new Vector3(0, (float)0.5, 0);
-    
-    private GameObject turret;
+    [Header("Optional")]
+    public GameObject turret;
     private Renderer rend;
     private Color startColor;
+    BuildManager buildManager;
     
     void Start ()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+        buildManager = BuildManager.instance;
+    }
+
+    public Vector3 GetBuildPosition()
+    {
+        return transform.position + positionOffset;
     }
     
     void OnMouseDown ()
     {
+        if (!buildManager.CanBuild)
+        {
+            return;
+        }
         if (turret != null)
         {
             Debug.Log("Can't build there! - TODO: Display on screen.");
             return;
         }
-
-        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
-        turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+        buildManager.BuildTurretOn(this);
     }
     void OnMouseEnter()
     {
+        if (!buildManager.CanBuild)
+        {
+            return;
+        }
         rend.material.color = hoverColor;
     }
     void OnMouseExit ()
--- Assets/Scripts/TurretBlueprint.cs
+++ Assets/Scripts/TurretBlueprint.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [System.Serializable]
 public class TurretBlueprint : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject prefab;
     public int cost;
 
