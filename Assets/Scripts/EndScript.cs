002ebab117fbf2a0deb4308b6158ec83206331a9
keithliu03 <keithliu2013@gmail.com>
2021-10-21 03:43:19 -0700

    Made build, fixed GUI for build, made game over show waves, made victory scene

--- Assets/Scripts/EndScript.cs
+++ Assets/Scripts/EndScript.cs
@@ -2,19 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text waveText;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        waveText.text = "You survived " + WaveSpawner.lastWave.ToString() + " waves.";
     }
 
     public void Return()
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WaveSpawner : MonoBehaviour
 {
@@ -14,7 +15,8 @@ public class WaveSpawner : MonoBehaviour
     public Text waveCountdownText;
     public Text waveIndexText;
 
-    private int waveIndex = 0;
+    public int waveIndex = 0;
+    public static int lastWave;
 
     void Update()
     {
@@ -31,8 +33,14 @@ public class WaveSpawner : MonoBehaviour
 
     IEnumerator SpawnWave()
     {
+        if (waveIndex > 34)
+        {
+            SceneManager.LoadScene("Victory");
+        }
+
         enemy.GetComponent<Enemy>().ScaleHP(waveIndex);
         waveIndex++;
+        lastWave = waveIndex;
         waveIndexText.text = "Wave: " + waveIndex.ToString();
         for (int i = 0; i < waveIndex; i++)
         {
