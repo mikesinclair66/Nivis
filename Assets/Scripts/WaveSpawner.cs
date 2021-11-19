30633162d8a9937681c0953f48b90102761fcf52
keithliu03 <38765153+keithliu03@users.noreply.github.com>
2021-11-18 21:58:25 -0800

    fixed last wave bug

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -38,36 +38,29 @@ public class WaveSpawner : MonoBehaviour
     {
         Debug.Log("WAVE INDEX: " + waveIndex);
         Debug.Log("WAVE LENGTH: " + waves.Length);
-        if (waveIndex > waves.Length)
+        if (waveIndex < waves.Length)
         {
-            SceneManager.LoadScene("Victory");
+            Wave wave = waves[waveIndex];
+
+            //enemy.GetComponent<Enemy>().ScaleHP();
+
+            waveIndexText.text = "Wave: " + waveIndex.ToString();
+            waveIndexText.text = "R" + waveIndex.ToString();
+
+            foreach (WaveEnemy i in wave.waveEnemies)
+            {
+                for (int j = 0; j < i.count; j++)
+                {
+                    SpawnEnemy(i.enemy);
+                    yield return new WaitForSeconds(1f / i.rate);
+                }
+            }
+
+            waveIndex++;
         }
         else
         {
-            if (waveIndex <= waves.Length)
-            {
-                Wave wave = waves[waveIndex];
-
-                //enemy.GetComponent<Enemy>().ScaleHP();
-
-                waveIndexText.text = "Wave: " + waveIndex.ToString();
-                waveIndexText.text = "R" + waveIndex.ToString();
-
-                foreach (WaveEnemy i in wave.waveEnemies)
-                {
-                    for (int j = 0; j < i.count; j++)
-                    {
-                        SpawnEnemy(i.enemy);
-                        yield return new WaitForSeconds(1f / i.rate);
-                    }
-                }
-
-                waveIndex++;
-            }
-
-
-            //Debug.Log("waveIndex: " + waveIndex);
-            //Debug.Log("waves.Length: " + waves.Length);
+            SceneManager.LoadScene("Victory");
         }
     }
 
